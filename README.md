1636d46aa2dbcecc5d1bca18d045cf758fdd77c9
Gabriel Laptop <gabiroom35@gmail.com>
2022-10-13 14:40:55 +0200

    finito mountainbiking

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 - [x] [Basic Programming 1](https://open.kattis.com/problems/basicprogramming1)
 - [x] [Longest Prime Sum](https://open.kattis.com/problems/longestprimesum)
 - [ ] [Money Matters](https://open.kattis.com/problems/moneymatters)
-- [ ] [Mountain Biking](https://open.kattis.com/problems/mountainbiking)
+- [x] [Mountain Biking](https://open.kattis.com/problems/mountainbiking)
 - [ ] [Ocean's Anti-11](https://open.kattis.com/problems/anti11)
 - [ ] [Zipline](https://open.kattis.com/problems/zipline)
 - [ ] [Secret Santa](https://open.kattis.com/problems/secretsanta)
---
+++ mountainbiking.java
@@ -0,0 +1,34 @@
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Scanner;
+
+    public class mountainbiking {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            final int N = sc.nextInt();
+            final double g = sc.nextDouble();
+
+            int[] distance = new int[N];
+            double[] angleD = new double[N];
+
+            for (int i = 0; i < N; i++) {
+                distance[i] = sc.nextInt();
+                angleD[i] = sc.nextInt();
+            }
+
+            double v = 0;
+
+            List<Double> listeV = new ArrayList<>();
+
+            for (int i = N-1; i >= 0; i--) {
+                //v+1 = sqrt(v*v + 2*g*cos(O)*d)
+                v = Math.sqrt(v*v + 2*g*Math.cos(Math.toRadians(angleD[i]))*distance[i]);
+                listeV.add(v);
+            }
+
+            for (int i = listeV.size()-1; i >= 0; i--) {
+                System.out.println(listeV.get(i));
+            }
+            sc.close();
+        }
+    }
