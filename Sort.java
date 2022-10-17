b43d2b07c52d8a77c09e9d7fc46f70fc8ed4af8e
Gabriel Laptop <gabiroom35@gmail.com>
2022-10-17 21:27:03 +0200

    fix de sort prcq Yves push de la merde

--- Sort.java
+++ Sort.java
@@ -1,13 +1,19 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Sort {
 
-    private static class Number {
+    private static class Nombre {
         int index;
         int occurence;
         int nombre;
 
-        public Number(int i, int n){
+        public Nombre(int i,int n){
             index = i;
             occurence = 0;
             nombre = n;
@@ -18,33 +24,34 @@ public class Sort {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        int c = sc.nextInt();
 
-        Map<Integer, Number> valeurs = new HashMap<>();
+        Map<Integer,Nombre> valeurs = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             int suivant = sc.nextInt();
-            Number number = valeurs.getOrDefault(suivant, new Number(i,suivant));
-            number.occurence++;
-            valeurs.put(suivant, number);
+            Nombre nombre = valeurs.getOrDefault(suivant, new Nombre(i,suivant));
+            nombre.occurence++;
+            valeurs.put(suivant, nombre);
         }
 
-        List<Number> listNumber = new ArrayList<>(valeurs.values());
-        Comparator<Number> byOccurence = (n1, n2) -> {
+        List<Nombre> listNombre = valeurs.values().stream().collect(Collectors.toList());
+        Comparator<Nombre> byOccurence = (n1,n2) -> {
             if(n1.occurence != n2.occurence) return n2.occurence - n1.occurence;
             return n1.index - n2.index;
         };
-        listNumber.sort(byOccurence);
+        Collections.sort(listNombre,byOccurence);
         
         StringBuilder sb = new StringBuilder();
-        for (Number number : listNumber) {
-            for (int i = 0; i < number.occurence; i++) {
-                sb.append(number.nombre).append(" ");
+        for (Nombre nombre : listNombre) {
+            for (int i = 0; i < nombre.occurence; i++) {
+                sb.append(nombre.nombre).append(" ");
             }
         }
 
         sb.deleteCharAt(sb.length()-1);
-        System.out.println(sb);
-
+        System.out.println(sb.toString());
         sc.close();
     }
 }
+
